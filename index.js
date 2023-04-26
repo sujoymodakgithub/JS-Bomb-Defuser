4619109cd522107200f5d744cd367df7bb990dba
sujoymodakgithub <modaksujoy74@gmail.com>
2023-04-26 12:39:38 +0000

    Bomb Defuser

---
+++ index.js
@@ -0,0 +1,27 @@
+let defuserel = document.getElementById("defuser");
+let timerEl = document.getElementById("timer");
+
+let countdown = 10;
+
+let intervalId = setInterval(function () {
+  countdown = countdown - 1;
+  timerEl.textContent = countdown;
+
+  if (countdown === 0) {
+    timerEl.textContent = "BOOM";
+    clearInterval(intervalId);
+  }
+}, 1000);
+
+dufuserEl.addEventListener("keydown", function (event) {
+  let bombDefuserText = defuserEl.value;
+
+  if (
+    event.key === "Enter" &&
+    bombDefuserText === "defuse" &&
+    countdown !== 0
+  ) {
+    timerEl.textContent = "You did it!";
+    clearInterval(intervalId);
+  }
+});
